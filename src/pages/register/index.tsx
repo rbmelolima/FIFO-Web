e6828d2af13681ac044f15cb085303ee29b14afe
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-01 21:37:54 -0300

    feat: criando estrutura da página de cadastro

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Container } from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <main>
+      <p>Cadastro</p>
+    </main>
+  );
+};
+
+export default Register;
