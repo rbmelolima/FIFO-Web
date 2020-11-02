91e66218093d401214637111a0c6a4e27b6455f1
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-01 21:37:39 -0300

    feat: criando estrutura da página de login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Container } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <main>
+      <p>Login</p>      
+    </main>
+  );
+};
+
+export default Login;
