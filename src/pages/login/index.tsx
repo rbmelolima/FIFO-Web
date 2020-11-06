5cfa700a7c00eafd46b2ebc857289ca3d973fdad
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-05 22:39:33 -0300

    temp: layout da tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Main, Title, Description, Subtitle } from './styles';
+import { Main, Title, Description, Subtitle, Label, Input } from './styles';
 
 const Login: React.FC = () => {
   return (
     <Main>
       <Title>Gerencie sua diversão</Title>
-      
+
       <Description>
         O Fifogame gestão de fila virtual tem a capacidade de organizar e controlar sua posição na fila de forma rápida e prática.
       </Description>
 
       <form action="">
         <Subtitle>Vamos começar</Subtitle>
+        <Label htmlFor="user-name">Digite seu nome</Label>
+        <Input placeholder="Nome: " required />
       </form>
     </Main>
   );
