a1d2e39497ba7be83624a3649406f8601baad746
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-09 14:44:30 -0300

    feat: barra de navegação

--- src/pages/chooseQueues/index.tsx
+++ src/pages/chooseQueues/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import CardQueue from '../../components/cardQueue';
 import * as Icons from './assets';
-import { WrapperButton, PageChooseQueue, Grid, Main } from './styles';
+import { WrapperButton, PageChooseQueue, Grid, Main, TitleChoiceButton, Navigation } from './styles';
+import AvatarNavbar from '../../components/avatarNavbar';
 
 const ChooseQueues: React.FC = () => {
   return (
     <PageChooseQueue>
-      <Main>
-        <h2 className="title-choice-button">Escolha a função</h2>
-        <WrapperButton>
+      <Navigation>
+        <AvatarNavbar name="Roger" url="https://avatars3.githubusercontent.com/u/48859060?s=460&u=123d876938f95e03eaadd31dbb079018fa374c77&v=4"/>
 
+        <div className="links">
+          <a href="/">FC-Santos</a>
+          <a href="/">FC-São Paulo</a>
+        </div>
+
+        <button>
+          <img src={Icons.menu} alt=""/>
+        </button>
+      </Navigation>
+
+      <Main>
+        <TitleChoiceButton> Escolha a função </TitleChoiceButton>
+
+        <WrapperButton>
           <div className="div-wrapper-button">
             <div className="box-button active-box-button">
               <button>
@@ -22,58 +36,61 @@ const ChooseQueues: React.FC = () => {
                 <h4>Minha Fila</h4>
               </button>
             </div>
-
           </div>
-
         </WrapperButton>
+
+        <div className="imgWrapperButton">
+          <img src={ Icons.entry } alt="" />
+        </div>
+
         <h2>Entrar na fila</h2>
         <Grid>
           <CardQueue
             name="Xadrez"
-            url={Icons.Xadrez}
-            click={() => { }}
+            url={ Icons.Xadrez }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Breanking Bad"
-            url={Icons.Breankingbad}
-            click={() => { }}
+            url={ Icons.Breankingbad }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Ping Pong"
-            url={Icons.Pingpong}
-            click={() => { }}
+            url={ Icons.Pingpong }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Resistence"
-            url={Icons.Resistance}
-            click={() => { }}
+            url={ Icons.Resistance }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Sinuca"
-            url={Icons.Sinuca}
-            click={() => { }}
+            url={ Icons.Sinuca }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Uno"
-            url={Icons.Uno}
-            click={() => { }}
+            url={ Icons.Uno }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Playstation"
-            url={Icons.Videogame}
-            click={() => { }}
+            url={ Icons.Videogame }
+            click={ () => { } }
           />
 
           <CardQueue
             name="Massagem"
-            url={Icons.Massagem}
-            click={() => { }}
+            url={ Icons.Massagem }
+            click={ () => { } }
           />
         </Grid>
       </Main>
--- src/pages/chooseQueues/styles.ts
+++ src/pages/chooseQueues/styles.ts
@@ -1,44 +1,84 @@
 import styled from 'styled-components';
 
+export const Navigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #fff;
 
+  .links {
+    margin: 0 8px;
 
+    a {
+      color: #fff;
+      border-bottom: 4px solid var(--color-primary-normal);
+      padding-bottom: 4px;
+    }
+
+    a:last-child{
+      margin-left: 16px;
+    }
+  }
+
+  button {
+    width: 26px;
+    height: 26px;
+    background: transparent;
+    outline: none;
+    border: none;
+  }
+`;
+
+export const TitleChoiceButton = styled.h2`
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 500; 
+  text-align: center;
+  color: #FDDCAE;
+  margin: 0;
+
+  @media screen and (max-width: 576px) {
+    .title-choice-button{
+      font-size: 20px;
+    }
+  }
+`;
 
 export const WrapperButton = styled.div`
-   
-    padding: 4.5px;
-    border-radius: 14px;
-    background: linear-gradient(
-      180deg,
-      #f4994d 0%,
-      #f4994d 0.01%,
-      rgba(244, 153, 77, 0.307222) 99.98%,
-      rgba(244, 153, 77, 0) 99.99%,
-      #786247 100%
-    );
-    width: min-content;
-    margin: 0 auto;
-    margin-top: 16px;
-
-.div-wrapper-button{
-  display:flex;
-  justify-content: space-between;
-  background-color: var(--color-background-dark);
-  width: 508px;
-  padding: 4px;
-  color: #fff;
-  font-size: 1.2rem;
-  border-radius: 10px;
-  outline: none;
-  border: none;
-}
-
-.box-button{
   padding: 4.5px;
   border-radius: 14px;
   width: min-content;
-}
+  margin: 0 auto 16px auto;
 
-.active-box-button {
+  background: linear-gradient(
+    180deg,
+    #f4994d 0%,
+    #f4994d 0.01%,
+    rgba(244, 153, 77, 0.307222) 99.98%,
+    rgba(244, 153, 77, 0) 99.99%,
+    #786247 100%
+  ); 
+
+  .div-wrapper-button{
+    display:flex;
+    justify-content: space-between;
+    background-color: var(--color-background-dark);
+    width: 508px;
+    padding: 4px;
+    color: #fff;
+    font-size: 1.2rem;
+    border-radius: 10px;
+    outline: none;
+    border: none;
+  }
+
+  .box-button{
+    padding: 4.5px;
+    border-radius: 14px;
+    width: min-content;
+  }
+
+  .active-box-button {
     background: linear-gradient(
       180deg,
       #f4994d 0%,
@@ -48,8 +88,6 @@ export const WrapperButton = styled.div`
       #786247 100%
     );
   }
-
-
 
   button {
     background-color: var(--color-background-dark);
@@ -62,38 +100,26 @@ export const WrapperButton = styled.div`
     border: none;
   }
 
-  h4{
-    font-family: Roboto;
+  h4 {    
     font-style: normal;
     font-weight: 500;
     font-size: 26px;
   }
-
  
   @media screen and (max-width: 576px) {
-    h4{
+    h4 {
       font-size: 14px;
-     }
-     .div-wrapper-button{
+    }
+
+    .div-wrapper-button{
       width: 248px;
-     }
+    }
   
-     button{
+    button{
       width: 109.77px;
-     }
+    }
   }
-  
-
-
 `;
-
-
-
-
-
-
-
-
 
 export const PageChooseQueue = styled.div`
   background-color: var(--color-background-dark);
@@ -105,6 +131,7 @@ export const PageChooseQueue = styled.div`
 export const Main = styled.main`
   max-width: 900px;
   margin: 0 auto;
+  padding: 64px 0;
 
   h2 {
     font-style: normal;
@@ -114,19 +141,10 @@ export const Main = styled.main`
     margin-bottom: 24px;
   }
 
-  .title-choice-button{
-    font-size:28px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 500; 
+  .imgWrapperButton {
+    width: 100%;
     text-align: center;
-    color: #FDDCAE;
-    margin:0;
-  }
-  @media screen and (max-width: 576px) {
-    .title-choice-button{
-      font-size:20px;
-    }
+    margin: 0 auto 60px auto;
   }
 `;
 
