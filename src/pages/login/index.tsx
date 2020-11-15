6ead1343a62fa4735339dc06b057fbdfb40d2589
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-15 14:57:54 -0300

    refactor: página de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import * as styles from './styles';
 
-const Login: React.FC = () => {  
+import Logo from '../../assets/logo/LOGO.svg';
+
+const Login: React.FC = () => {
 
   return (
-    <styles.PageLogin>
-    
-    </styles.PageLogin>
+    <>
+      <styles.Navbar>
+        <img src={ Logo } alt="Fila" />
+        <styles.ButtonSecondary>
+          Como funciona
+        </styles.ButtonSecondary>
+      </styles.Navbar>
+
+      <styles.PageLogin>
+        <styles.Main>
+          <h1>  Gerencie sua diversão </h1>
+          <p>
+            O Fila virtual tem a capacidade de organizar e controlar sua posição de forma rápida e prática.
+          </p>
+
+          <h2>Vamos começar!</h2>
+
+          <input type="text" placeholder="email" />
+
+          <styles.ButtonPrimary>
+            Entrar
+          </styles.ButtonPrimary>
+        </styles.Main>
+
+      </styles.PageLogin>
+    </>
   )
 };
 
