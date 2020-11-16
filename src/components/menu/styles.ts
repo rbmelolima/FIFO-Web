e56dec4ca49e544b02d7ed01abb136b511d6b601
Maria Vitória Durazzo <marividurazzo007@gmail.com>
2020-11-16 01:50:15 -0300

    feat: codigo do menu

--- src/components/menu/styles.ts
+++ src/components/menu/styles.ts
@@ -64,9 +64,6 @@ export const Container = styled.div`
        
     }
 
-    @media screen and (max-height: 450px) {
-        .sidenav {padding-top: 15px;}
-        .sidenav a {font-size: 18px;}
-    }
+   
       
 `;
