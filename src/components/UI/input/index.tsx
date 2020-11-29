f6418fe6deb691516f92bd452cf04a58af7b3c86
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-29 17:49:01 -0300

    refactor: externalizando o placeholder no input

--- src/components/UI/input/index.tsx
+++ src/components/UI/input/index.tsx
@@ -2,11 +2,12 @@ import { TextFieldProps } from '@material-ui/core/TextField/TextField';
 import React from 'react';
 import { InputMaterial } from './styles';
 
-const Input: React.FC<TextFieldProps> = ({ label, ...rest }) => {
+const Input: React.FC<TextFieldProps> = ({ label, placeholder, ...rest }) => {
   return (
     <InputMaterial
-      { ...rest }      
-      label={label}
+      { ...rest }
+      label={ label }
+      placeholder={ placeholder }
       variant="outlined"
     />
   );
