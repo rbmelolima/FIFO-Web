1c541597b11ead46468ffbc6a9db9c7339842e9d
Maria Vitória Durazzo <marividurazzo007@gmail.com>
2020-11-29 03:59:46 -0300

    refactor: form adicionado ao index

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,25 +4,45 @@ import section_2 from '../../assets/about/section_2_index.png';
 import section_3 from '../../assets/about/section_3_index.png';
 import section_4 from '../../assets/about/section_4_index.png';
 import * as styles from './styles';
+import Logo from '../../assets/logo/logo.svg';
+import { ButtonPrimary, ButtonSecondary } from '../../styles/buttons';
+import Input from '../../components/UI/input/index';
+
 
 
 
 function About() {
     return (
-        <styles.Main>
-            <section>
-                <img src={section_1} alt="Gerencie sua diversão - O Fila virtual tem a capacidade de organizar e controlar sua posição na fila de forma rápida e prática." />
-            </section>
-            <section>
-                <img src={section_2} alt="Ambiente organizado para sua diversão - No Fila você encontra Serviços disponiveis para sua diversão da empresa de forma fácil em um só lugar." />
-            </section>
-            <section>
-                <img src={section_3} alt="Aqui é facil de entrar na fila - Entre na fila das diversões,  aqui você consegue ver quantas pessoas tem em cada serviço podendo escolher a que está com a fila menor para se divertir mais rápido." />
-            </section>
-            <section>
-                <img src={section_4} alt="Sua notificação - Você consegue ver qual posição está atrávez do nosso relatório te informando quando chegar sua vez." />
-            </section>
-        </styles.Main>
+        <>
+            <styles.Navbar>
+                <img src={Logo} alt="Fila" />
+                <ButtonSecondary>
+                    Como funciona
+            </ButtonSecondary>
+            </styles.Navbar>
+
+            <styles.Main>
+                <section>
+                    <img src={section_1} alt="Gerencie sua diversão - O Fila virtual tem a capacidade de organizar e controlar sua posição na fila de forma rápida e prática." />
+                </section>
+                <section>
+                    <img src={section_2} alt="Ambiente organizado para sua diversão - No Fila você encontra Serviços disponiveis para sua diversão da empresa de forma fácil em um só lugar." />
+                </section>
+                <section>
+                    <img src={section_3} alt="Aqui é facil de entrar na fila - Entre na fila das diversões,  aqui você consegue ver quantas pessoas tem em cada serviço podendo escolher a que está com a fila menor para se divertir mais rápido." />
+                </section>
+                <section>
+                    <img src={section_4} alt="Sua notificação - Você consegue ver qual posição está atrávez do nosso relatório te informando quando chegar sua vez." />
+                </section>
+                <section className="formIndex">
+                    <h2>Vamos começar sua diversão?</h2>
+                    <Input label="Email" name="email-input" />
+                    <ButtonPrimary>
+                        Entrar
+                    </ButtonPrimary>
+                </section>
+            </styles.Main>
+        </>
     );
 }
 
--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -6,8 +6,74 @@ export const Main = styled.main`
     box-sizing: border-box;
     text-decoration: none;
     background-color: #222222;
+    
+
     section img{
         width:100%;
-        height:600px;
+        height:700px;
     }
+
+
+    h2 {
+        background-image: linear-gradient(180deg, #FE662E 17.76%, #F59C33 62.55%);
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent; 
+        -moz-background-clip: text;
+        -moz-text-fill-color: transparent;
+        font-weight: 500;
+        font-size: 26px;
+        line-height: 28px;
+        text-align: center;
+        
+        margin-bottom: 20px;
+      }
+    
+      input {
+        margin-bottom: 16px;
+        max-width: 460px;
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    
+      button {
+        max-width: 300px;
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top:20px;
+      }
+
+      .formIndex{
+          margin-top:100px;
+          display:flex;
+          flex-direction: column;
+          justify-content: center;
+      }
+    
+`;
+
+export const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  gap: 16px;
+  background-color: var(--color-background);
+  border-bottom: 1px solid var(--color-primary-light);
+
+  img {
+    width: 80px;
+    height: auto;
+  }
+
+
+  @media screen and (min-width: 748px) {
+    padding: 16px 32px;
+
+    img {
+      width: 100px;
+    }
+  }
 `;
