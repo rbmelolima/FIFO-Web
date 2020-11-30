9180f93a8c42a589cd4e909e06c8566bba8baf28
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-29 22:55:32 -0300

    refactor: removendo o context de usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import UserProvider from '../entities/user/context';
 
 import Queues from '../pages/queues';
 import Index from '../pages/index';
 
 export default function Routes () {
   return (
-    <UserProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Index />
-          </Route>          
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Index />
+        </Route>
 
-          <Route path="/filas">
-            <Queues />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </UserProvider>
+        <Route path="/filas">
+          <Queues />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
